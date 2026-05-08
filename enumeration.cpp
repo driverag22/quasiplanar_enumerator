5b770eee905d70c556f3a5f03c202cb16563cc1d
driverag22 <driverag22@gmail.com>
2026-05-08 13:52:02 +0200

    cluster updates

--- enumeration.cpp
+++ enumeration.cpp
@@ -20,15 +20,16 @@ Edges generateCompleteGraph(std::size_t n) {
 }
 
 const std::size_t n = 11;
+const std::string split = "211";
 const Edges edges =
 {
-    {0,1},{0,2},{0,3},{0,4},{0,5},{0,6},{0,7},{0,8},{0,9},{0,10},
-    {1,2},{1,3},{1,4},{1,5},{1,6},{1,7},{1,8},{1,9},{1,10},
-    {2,3},{2,4},{2,5},{2,6},{2,7},{2,8},{2,9},{2,10},
+    {0,1},{0,2},{0,3},{0,4},{0,5},{0,6},{0,7},{0,8},//{0,9},{0,10},
+    {1,2},{1,3},{1,4},{1,5},{1,6},{1,7},{1,8},{1,9},//{1,10},
+    {2,3},{2,4},{2,5},{2,6},{2,7},{2,8},{2,9},//{2,10},
     {3,4},{3,5},{3,6},{3,7},{3,8},{3,9},{3,10},
-    {4,5},{4,6},{4,7},{4,8},{6,9},{6,10},
+    {4,5},{4,6},{4,7},{4,8},{4,9},{4,10},
     {5,6},{5,7},{5,8},{5,9},{5,10},
-    //{6,7},{6,8},{6,9},{6,10},
+    {6,7},{6,8},{6,9},{6,10},
     {7,8},{7,9},{7,10},
     {8,9},{8,10},
     {9,10}
@@ -39,23 +40,25 @@ const std::size_t klim = 8;
 int main() {
     std::cout << "\n\n ===================================================== \n";
     //const Edges edges = generateCompleteGraph(n);
-    std::cout << "k = " << klim << ", n = " << n << "\n" << std::endl;
+    std::cout << "k = " << klim << ", n = " << n << ", split: " << split << std::endl;
+    //std::cout << "k = " << klim << ", n = " << n << std::endl;
     std::size_t minimal_cr = 0x3f3f3f3f;
     std::vector< Drawing<klim> > solutions;
     Drawing<klim> d(n);
     d.add_first_edge(edges[0][0], edges[0][1]);
-    std::size_t num_fixed_edges = n;
-    for (std::size_t i = 2; i < num_fixed_edges; ++i) {
-        HdsPath p = d.first_path(0, i);
-        if (p.empty()) {
-            throw std::runtime_error("Failed to build the initial star!");
-        }
-        d.add_edge(p, i);
-    }
-    std::cout << "Star built" << std::endl;
+    //std::size_t num_fixed_edges = n;
+    //for (std::size_t i = 2; i < num_fixed_edges; ++i) {
+    //    HdsPath p = d.first_path(0, i);
+    //    if (p.empty()) {
+    //        throw std::runtime_error("Failed to build the initial star!");
+    //    }
+    //    d.add_edge(p, i);
+    //}
+    //std::cout << "Star built" << std::endl;
 
-    auto start_edge = edges.begin() + (num_fixed_edges-1);
-    //auto start_edge = edges.begin() + 1;
+    //auto start_edge = edges.begin() + (num_fixed_edges-1);
+    auto start_edge = edges.begin() + 1;
+    //int counter = 0;
 
     for (auto e = start_edge;;) {
         std::size_t u = (*e)[0];
@@ -80,11 +83,9 @@ BACKUP:
         }
         d.add_edge(p, v);
         if (++e == edges.end()) {
-            bool is_new = true;
-            if (is_new) {
-                solutions.push_back(d);
-                minimal_cr = std::min(minimal_cr,d.crossings.size());
-            }
+            solutions.push_back(d);
+            minimal_cr = std::min(minimal_cr,d.crossings.size());
+            std::cout << "found sol\n";
             goto BACKUP;
         }
     }
@@ -117,12 +118,21 @@ END:
             if(is_unique) {
                 if (!it->verify_quasiplanarity()) {
                     std::cerr << "CRITICAL ERROR: Drawing is not 3-quasiplanar!" << std::endl;
+                    solutions_mincr_uni.push_back((*it));
+                    std::ofstream of;
+                    std::ostringstream filename;
+                    filename << "drawings/fail_Drawing_S" << n << "_k" << klim << "_" << solutions_mincr_uni.size() << ".graphml";
+                    //filename << "drawings/K11_minus_4/NOTQUASI_" << split << "_drawing_K" << n << "_" << klim << "_" << solutions_mincr_uni.size() << ".graphml";
+                    of.open(filename.str());
+                    (*it).graphml_output(of);
+                    of.close();
                 } else {
                     solutions_mincr_uni.push_back((*it));
                     std::ofstream of;
                     std::ostringstream filename;
-                    //filename << "drawings/Drawing_K" << n << "_k" << klim << "_" << solutions_mincr_uni.size() << ".graphml";
-                    filename << "drawings/K11_minus_4/fixed_drawing_K" << n << "_" << klim << "_" << solutions_mincr_uni.size() << ".graphml";
+                    filename << "drawings/Drawing_S" << n << "_k" << klim << "_" << solutions_mincr_uni.size() << ".graphml";
+                    //filename << "drawings/K11_minus_4/4_drawing_K" << n << "_" << klim << "_" << solutions_mincr_uni.size() << ".graphml";
+                    //filename << "drawings/K11_minus_4/" << split << "_drawing_K" << n << "_" << klim << "_" << solutions_mincr_uni.size() << ".graphml";
                     of.open(filename.str());
                     (*it).graphml_output(of);
                     of.close();
