55642523727dfd298c457045880514744d6e7bd3
diego <driverag22@gmail.com>
2026-06-04 10:31:51 +0200

    K11 minus 4 enumerator starting from K7 quasiplane drawings

--- K11_minus_4.cpp
+++ K11_minus_4.cpp
@@ -1,109 +1,80 @@
 #include "hds_quasiplanar.h"
-#include "iso.h"
-#include <sstream>
 #include <fstream>
 
 typedef std::vector<std::size_t> Edge;
 typedef std::vector<Edge> Edges;
 
-const std::size_t n = 11;
-const std::size_t klim = 9;
+const std::size_t n = 11; // note hard-coded limit of 64 edges for quasiplanar...
+const std::string split = "4i";
+const Edges edges =
+{
+    {0,7},{2,7},{3,7},{4,7},{5,7},{6,7},{7,8},{7,9},{7,10},
+    {0,8},{1,8},{3,8},{4,8},{5,8},{6,8},{8,9},{8,10},
+    {0,9},{1,9},{2,9},{4,9},{5,9},{6,9},{9,10},
+    {0,10},{1,10},{2,10},{3,10},{5,10},{6,10}
+};
+
+const std::size_t klim = 11;
 
 int main() {
-    std::string name = "jsons/K10/K10_plus_one";
-    std::string filename = name + ".json";
-    std::ifstream input_file(filename);
-    nlohmann::json import_data;
-    input_file >> import_data;
-    input_file.close();
+    std::cout << "\n\n ===================================================== \n";
+    std::cout << "k = " << klim << ", n = " << n  << ", split = " << split << std::endl;
 
-    // loading drawing
-    Drawing<klim> loaded_drawing(import_data);
+    for (int i = 0; i < 1607; i++) {
+        std::string name = "./drawings/K7_k9/jsons/" + std::to_string(i);
+        std::string jsonFile = name + ".json";
+        std::ifstream input_file(jsonFile);
+        nlohmann::json import_data;
+        input_file >> import_data;
+        input_file.close();
 
-    const Edges edges = {
-        {1,10},{5,10},{7,10},{8,10},{9,10}
-    };
+        // loading drawing
+        Drawing<klim> d(import_data);
+        auto start_edge = edges.begin();
 
-    std::vector< Drawing<klim> > solutions;
-    std::vector<std::size_t> d_cnt(300,1); // assume no more than 300 unique drawings up to iso
-    std::size_t counter = 1;
+        for (auto e = start_edge;;) {
+            std::size_t u = (*e)[0];
+            std::size_t v = (*e)[1];
+            HdsPath p = d.first_path(u, v);
+            if (p.empty()) {
+                // no way to add uv -> do previous edges differently
+                do {
+                    if (e == start_edge) {
+                        goto END;
+                    }
+                    --e;
 
-    auto start_edge = edges.begin();
-    for (auto e = start_edge;;) {
-        std::size_t u = (*e)[0];
-        std::size_t v = (*e)[1];
-        HdsPath p = loaded_drawing.first_path(u, v);
-        if (p.empty()) {
-BACKUP:
-            // no way to add uv -> do previous edges differently
-            do {
-                if (e == start_edge) {
-                    goto END;
-                }
-                --e;
-
-                u = (*e)[0];
-                assert(u == loaded_drawing.edges.back().u);
-                v = (*e)[1];
-                assert(v == loaded_drawing.edges.back().v);
-                p = loaded_drawing.edges.back().built;
-                loaded_drawing.remove_edge();
-            } while (!loaded_drawing.next_path(p, v));
-        }
-        loaded_drawing.add_edge(p, v);
-
-        if (++e == edges.end()) {
-            bool newSol = true;
-            std::size_t d_ind = 0;
-            for (auto it = solutions.begin(); it != solutions.end(); it++) {
-                if(are_isomorphic((*it),loaded_drawing)) {
-                    newSol = false;
-                    d_cnt[d_ind]++;
-                    break;
-                }
-                d_ind++;
+                    u = (*e)[0];
+                    assert(u == d.edges.back().u);
+                    v = (*e)[1];
+                    assert(v == d.edges.back().v);
+                    p = d.edges.back().built;
+                    d.remove_edge();
+                } while (!d.next_path(p, v));
             }
-            if (newSol) {
-                if (!loaded_drawing.verify_quasiplanarity()) {
-                } else {
-                    solutions.push_back(loaded_drawing);
-                    std::cout << ++counter << std::endl;
-                    goto BACKUP;
-                }
+            d.add_edge(p, v);
+
+            if (++e == edges.end()) {
+                std::cout << "Found sol for: " << split << std::endl;
+
+                // output json
+                std::string name = "drawings/K11_minus_4/" + split + "/" + std::to_string(i);
+                std::string jsonOut = name + ".json";
+                std::ofstream of_json(jsonOut);
+                nlohmann::ordered_json output_json = d.serialize_to_json();
+                of_json << output_json.dump(4);
+                of_json.close();
+
+                // output graphml
+                std::string graphmlOut = name + ".graphml";
+                std::ofstream of_graphml(graphmlOut);
+                d.graphml_output(of_graphml);
+                of_graphml.close();
+                return 0;
             }
-            goto BACKUP;
         }
     }
 END:
-    std::cout << "Found " << solutions.size() << " drawings in total." << std::endl;
-    if(solutions.size() == 0) {
-        return 0;
-    }
-
-    std::size_t idx = 0;
-    for (auto it = solutions.begin();it!=solutions.end();it++) {
-        std::string filename = "jsons/K11_minus_4/star/star_" + std::to_string(idx) + ".json";
-        std::ofstream of_json(filename);
-        nlohmann::ordered_json output_json = (*it).serialize_to_json();
-        of_json << output_json.dump(4);
-        of_json.close();
-
-        std::string filename2 = "jsons/K11_minus_4/star/star_" + std::to_string(idx) + ".graphml";
-        std::ofstream of_graphml(filename2);
-        (*it).graphml_output(of_graphml);
-        of_graphml.close();
-
-        idx++;
-    }
-
-    std::cout << "Found " << counter << " drawings in total." << std::endl;
-    std::cout << "Found " << solutions.size() << " unique drawings in total." << std::endl;
-    // std::cout << "Minimal Crossing Number is "<<minimal_cr<<std::endl;
-
-    for (std::size_t i = 0; i < solutions.size(); i++) {
-        std::cout << "Drawing-" << i << " has " << d_cnt[i] << " isomorphic drawings" << std::endl;
-    }
-    return 0;
-
+    std::cout << "Found no solutions with k = " << klim << " for split = " << split << std::endl;
     return 0;
 }
