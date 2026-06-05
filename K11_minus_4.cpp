55520b58b66a58a7641aca550ebf2429245a8d15
diego <driverag22@gmail.com>
2026-06-05 10:40:14 +0200

    fixed K11 min 4 enumerator, setup star configuration

--- K11_minus_4.cpp
+++ K11_minus_4.cpp
@@ -5,23 +5,24 @@ typedef std::vector<std::size_t> Edge;
 typedef std::vector<Edge> Edges;
 
 const std::size_t n = 11; // note hard-coded limit of 64 edges for quasiplanar...
-const std::string split = "4i";
+const std::string split = "star";
 const Edges edges =
 {
-    {0,7},{2,7},{3,7},{4,7},{5,7},{6,7},{7,8},{7,9},{7,10},
-    {0,8},{1,8},{3,8},{4,8},{5,8},{6,8},{8,9},{8,10},
-    {0,9},{1,9},{2,9},{4,9},{5,9},{6,9},{9,10},
-    {0,10},{1,10},{2,10},{3,10},{5,10},{6,10}
+    {0,7},{2,7},{3,7},{4,7},{5,7},{6,7},
+    {0,8},{1,8},{2,8},{3,8},{4,8},{5,8},{6,8},{8,9},{8,10},
+    {0,9},{1,9},{2,9},{3,9},{4,9},{5,9},{6,9},{9,10},
+    {0,10},{1,10},{2,10},{3,10},{4,10},{5,10},{6,10}
 };
 
-const std::size_t klim = 11;
+const std::size_t klim = 9;
 
 int main() {
     std::cout << "\n\n ===================================================== \n";
     std::cout << "k = " << klim << ", n = " << n  << ", split = " << split << std::endl;
 
     for (int i = 0; i < 1607; i++) {
-        std::string name = "./drawings/K7_k9/jsons/" + std::to_string(i);
+        std::cout << i << std::endl;
+        std::string name = "../quasiDrawings/K7/klim9_jsons/" + std::to_string(i);
         std::string jsonFile = name + ".json";
         std::ifstream input_file(jsonFile);
         nlohmann::json import_data;
@@ -40,7 +41,7 @@ int main() {
                 // no way to add uv -> do previous edges differently
                 do {
                     if (e == start_edge) {
-                        goto END;
+                        goto NEXT_JSON;
                     }
                     --e;
 
@@ -56,9 +57,10 @@ int main() {
 
             if (++e == edges.end()) {
                 std::cout << "Found sol for: " << split << std::endl;
+                if (!d.verify_quasiplanarity()) std::cout << "not quasi?\n";
 
                 // output json
-                std::string name = "drawings/K11_minus_4/" + split + "/" + std::to_string(i);
+                std::string name = "../quasiDrawings/K11_minus_4/" + split + "/klim" + std::to_string(klim) + "_" + std::to_string(i);
                 std::string jsonOut = name + ".json";
                 std::ofstream of_json(jsonOut);
                 nlohmann::ordered_json output_json = d.serialize_to_json();
@@ -73,8 +75,8 @@ int main() {
                 return 0;
             }
         }
+NEXT_JSON:;
     }
-END:
     std::cout << "Found no solutions with k = " << klim << " for split = " << split << std::endl;
     return 0;
 }
