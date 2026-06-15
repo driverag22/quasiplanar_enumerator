f8dde9e02d42b8779455e1b09274e033479ab0a4
diego <driverag22@gmail.com>
2026-06-15 10:07:20 +0200

    stopped tracking compile.sh

--- compile.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# -O3 turns on maximum performance optimizations
-# -std=c++17 ensures modern C++ features are supported
-# -Wall shows standard warnings to help catch bugs
-g++ -O3 -Wall -std=c++17 enumeration.cpp -o ./.bin/enumeration
-
-./.bin/enumeration
