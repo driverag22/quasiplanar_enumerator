0898d7797a8ffb01fb0e3c47dd3cdf1ed3407da8
diego <driverag22@gmail.com>
2026-07-28 17:56:45 +0200

    code to verify/test maximality of (star) drawings

--- starVerifyMax.cpp
+++ starVerifyMax.cpp
@@ -6,65 +6,66 @@
 typedef std::vector<std::size_t> Edge;
 typedef std::vector<Edge> Edges;
 
-const std::size_t n = 12; // 58 edges for optimal quasi
-const Edges edges = // 57 + 1 = 58
-{
-    // for bistar_eq
-    // {6,10},{7,10},{8,10},{9,10},
-    // {5,11},{6,11},{7,11},{8,11},{9,11},
+const std::size_t n = 14;
+const std::size_t klim = 21; // leq 2n-7=21
 
-    // for bistar_sep
-    {6,10},{7,10},{8,10},{9,10},
-    {0,11},{1,11},{2,11},{3,11},
-    {10,11},
-};
+// Helper function to compute missing edges for a drawing
+std::vector<std::pair<std::size_t, std::size_t>> get_missing_edges(const Drawing<klim>& d, std::size_t num_vertices) {
+    std::vector<std::vector<bool>> adj(num_vertices, std::vector<bool>(num_vertices, false));
+    for (const auto& edge : d.edges) {
+        adj[edge.u][edge.v] = true;
+        adj[edge.v][edge.u] = true;
+    }
 
-const std::size_t klim = 17; // leq 2n-7=17
+    std::vector<std::pair<std::size_t, std::size_t>> missing;
+    for (std::size_t u = 0; u < num_vertices; ++u)
+        for (std::size_t v = u + 1; v < num_vertices; ++v)
+            if (!adj[u][v]) missing.push_back({u, v});
+    return missing;
+}
+
+bool is_drawing_extendable(const Drawing<klim>& d, std::size_t num_vertices) {
+    auto missing_edges = get_missing_edges(d, num_vertices);
+
+    for (const auto& [u, v] : missing_edges) {
+        Drawing<klim> d_search(d);
+        HdsPath p = d_search.first_path(u, v);
+
+        while (!p.empty()) {
+            Drawing<klim> d_test(d);
+            d_test.add_edge(p, v);
+            if (d_test.verify_quasiplanarity()) {
+                std::cout << "\n  [!] Edge (" << u << ", " << v << ") can be legally added!";
+                return true;
+            }
+            if (!d_search.next_path(p, v)) {
+                break;
+            }
+        }
+    }
+    return false;
+}
 
 int main() {
-    std::vector< Drawing<klim> > solutions;
-    std::size_t counter = 0;
-    std::cout << "\n\n ===================================================== \n star\n";
-    std::cout << "k = " << klim << ", n = " << n  << std::endl;
+    bool graph_is_maximal = true;
 
-    for (int i = 0; i < 6; i++) {
-        std::cout << "56 edge drawing " << i << std::endl;
-        std::string name = "../quasiDrawings/maxQuasi/K12_min_bistar_sep/" + std::to_string(i);
-        std::string jsonFile = name + ".json";
-        std::ifstream input_file(jsonFile);
+    for (int i = 0; i < 18; i++) {
+        std::cout << "Drawing " << i << std::endl;
+        std::string filename = "../quasiDrawings/maxQuasi/14_with_K4/" + std::to_string(i) + ".json";
+        std::ifstream input_file(filename);
         nlohmann::json import_data;
         input_file >> import_data;
         input_file.close();
 
-        // loading drawing
         Drawing<klim> d(import_data, n);
-        std::size_t eC = 0;
-        for (const auto& e : edges) {
-            std::cout << eC++ << std::endl;
-            std::size_t u = e[0];
-            std::size_t v = e[1];
-            
-            HdsPath p = d.first_path(u, v);
-            // check all possible paths for this single edge
-            while (!p.empty()) {
-                d.add_edge(p,v);
-                if (d.verify_quasiplanarity()) {
-                    std::cout << "  -> SUCCESS: Added edge " << u << "-" << v << ". Graph is NOT maximal." << std::endl;
-                    solutions.push_back(d); 
-                    std::cout << counter++ << std::endl;
-                }
-                d.remove_edge();
 
-                if (!d.next_path(p,v)) {
-                    break;
-                }
-            }
+        if (is_drawing_extendable(d, n)) {
+            std::cout << " -> EXTENDABLE (Not maximal)\n";
+            graph_is_maximal = false;
+        } else {
+            std::cout << " -> MAXIMAL\n";
         }
     }
-     if (solutions.size() == 0) 
-         std::cout << "Maximal quasiplanar"  << std::endl;
-     else 
-         std::cout << "Not max quasiplanar!" << std::endl; 
 
     return 0;
 }
