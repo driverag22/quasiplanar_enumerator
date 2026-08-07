1e45ddd0d2b770dd6f3fc9506e15fc46c5f928c2
diego <driverag22@gmail.com>
2026-08-07 11:46:58 +0200

    fixed K12 min 8 enumerator to check all permutations

--- K12_minus_8.cpp
+++ K12_minus_8.cpp
@@ -1,38 +1,68 @@
 #include "hds_quasiplanar.h"
 #include <fstream>
+#include <set>
 
 typedef std::vector<std::size_t> Edge;
 typedef std::vector<Edge> Edges;
 
 const std::size_t n = 12; // note hard-coded limit of 64 edges for quasiplanar...
-const std::string split = "1";
-const Edges edges = // 57 edges
-{
-    {0,7},{1,7},{2,7},{3,7},{4,7},
-    // {5,7},
-    {1,8},{2,8},{3,8},{4,8},{5,8},{6,8},
-    {0,9},{1,9},{2,9},{3,9},{4,9},
-    {1,10},{2,10},{3,10},{4,10},{5,10},
-    {2,11},{3,11},{4,11},{5,11},{6,11},
-    // K5
-    {7,8},{7,9},{7,10},{7,11},
-    {8,9},{8,10},{8,11},
-    {9,10},{9,11},
-    {10,11},
-};
-
-const std::vector<std::pair<std::size_t,std::size_t>> missingEdges =
-{
-    {5,7},
-    {6,7},
-    {0,8},
-    {5,9},{6,9},
-    {0,10},{6,10},
-    {0,11},{1,11},
-};
 const std::size_t klim = 17; // 2n-7 = 17
+const std::string split = "1";
 
-bool is_drawing_extendable(const Drawing<klim>& d, std::size_t num_vertices) {
+struct EdgeSetWithMissing {
+    Edges edges;
+    std::vector<std::pair<std::size_t, std::size_t>> missing_edges;
+};
+
+std::vector<EdgeSetWithMissing> generate_edge_sets() {
+    const bool r_masks[4][5] = {
+        {1, 0, 1, 0, 0}, // ra: {7, 9}
+          {1, 1, 1, 1, 0}, // rb: {7, 8, 9, 10}
+          {0, 1, 0, 1, 1}, // rc: {8, 10, 11}
+          {0, 1, 0, 0, 1}  // rd: {8, 11}
+    };
+    std::vector<EdgeSetWithMissing> all_edge_sets;
+
+    // 7 choose 3 mask to select the 3 vertices connected to all {7,8,9,10,11}
+    std::vector<int> mask(7, 0);
+    std::fill(mask.end() - 3, mask.end(), 1);
+
+    do {
+        Edges base_edges = {
+        {7,8},{7,9},{7,10},{7,11},
+        {8,9},{8,10},{8,11},
+        {9,10},{9,11},
+        {10,11}
+        };
+        std::vector<std::size_t> C; // 3 vertices connected to 7,8,9,10,11
+        std::vector<std::size_t> R; // 4 remaining vertices
+        for (std::size_t i = 0; i < 7; ++i) {
+            if (mask[i]) for (std::size_t j=7;j<12;++j) base_edges.push_back({i,j});
+            else R.push_back(i);
+        }
+
+        do {
+            EdgeSetWithMissing item;
+            item.edges = base_edges;
+
+            for (std::size_t j = 0; j < 5; ++j) {
+                std::size_t target = 7 + j;
+                for (std::size_t k = 0; k < 4; ++k) {
+                    if (r_masks[k][j]) item.edges.push_back({R[k], target});
+                    else item.missing_edges.push_back({R[k], target});
+                }
+            }
+
+            std::sort(item.edges.begin(), item.edges.end());
+            all_edge_sets.push_back(item);
+
+        } while (std::next_permutation(R.begin(), R.end()));
+    } while (std::next_permutation(mask.begin(), mask.end()));
+    return all_edge_sets;
+}
+
+bool is_drawing_extendable(const Drawing<klim>& d, 
+        const std::vector<std::pair<std::size_t, std::size_t>>& missingEdges) {
     for (const auto& [u,v]: missingEdges) {
         Drawing<klim> d_search(d);
         HdsPath p = d_search.first_path(u,v);
@@ -56,62 +86,58 @@ int main() {
 
     for (int i = 0; i < 1607; i++) {
         std::cout << "Drawing " << std::to_string(i) << std::endl;
-        std::string name = "../quasiDrawings/K7/klim9_jsons/" + std::to_string(i);
-        std::string jsonFile = name + ".json";
-        std::ifstream input_file(jsonFile);
-        nlohmann::json import_data;
-        input_file >> import_data;
-        input_file.close();
 
+        std::ifstream input_file("../quasiDrawings/K7/klim9_jsons/" + std::to_string(i) + ".json");
+        nlohmann::json import_data; input_file >> import_data; input_file.close();
         // loading drawing
         Drawing<klim> d(import_data, n);
-        auto start_edge = edges.begin();
 
-        for (auto e = start_edge;;) {
-            std::size_t u = (*e)[0];
-            std::size_t v = (*e)[1];
-            HdsPath p = d.first_path(u, v);
-            if (p.empty()) {
-                // no way to add uv -> do previous edges differently
-                do {
-                    if (e == start_edge) {
-                        goto NEXT_JSON;
+        std::vector<EdgeSetWithMissing> all_edge_sets = generate_edge_sets();
+        std::size_t total_edge_sets = all_edge_sets.size();
+
+        int idx = 0;
+        for (const auto& item : all_edge_sets) {
+            std::cout << "\rPermutation [" << idx << " / " << total_edge_sets << "]" << std::flush;
+            const Edges& current_edges = item.edges;
+            const auto& missing_edges = item.missing_edges;
+
+            auto start_edge = current_edges.begin();
+
+            for (auto e = start_edge;;) {
+                std::size_t u = (*e)[0];
+                std::size_t v = (*e)[1];
+                HdsPath p = d.first_path(u, v);
+                if (p.empty()) {
+                    // no way to add uv -> do previous edges differently
+                    do {
+                        if (e == start_edge) {
+                            goto NEXT_JSON;
+                        }
+                        --e;
+
+                        u = (*e)[0];
+                        assert(u == d.edges.back().u);
+                        v = (*e)[1];
+                        assert(v == d.edges.back().v);
+                        p = d.edges.back().built;
+                        d.remove_edge();
+                    } while (!d.next_path(p, v));
+                }
+                d.add_edge(p, v);
+
+                if (++e == current_edges.end()) {
+                    std::cout << "Found sol for: " << split << std::endl;
+                    if (!d.verify_quasiplanarity()) std::cout << "not quasi?\n";
+                    if (is_drawing_extendable(d, missing_edges)) {
+                        std::cout << "extendable\n";
+                        return 0;
                     }
-                    --e;
-
-                    u = (*e)[0];
-                    assert(u == d.edges.back().u);
-                    v = (*e)[1];
-                    assert(v == d.edges.back().v);
-                    p = d.edges.back().built;
-                    d.remove_edge();
-                } while (!d.next_path(p, v));
-            }
-            d.add_edge(p, v);
-
-            if (++e == edges.end()) {
-                std::cout << "Found sol for: " << split << std::endl;
-                if (!d.verify_quasiplanarity()) std::cout << "not quasi?\n";
-                if (is_drawing_extendable(d, n)) std::cout << "extendable\n";
-
-                // output json
-                // std::string name = "../quasiDrawings/K11_minus_4/" + split + "/klim" + std::to_string(klim) + "_" + std::to_string(i);
-                // std::string jsonOut = name + ".json";
-                // std::ofstream of_json(jsonOut);
-                // nlohmann::ordered_json output_json = d.serialize_to_json();
-                // of_json << output_json.dump(4);
-                // of_json.close();
-
-                // // output graphml
-                // std::string graphmlOut = name + ".graphml";
-                // std::ofstream of_graphml(graphmlOut);
-                // d.graphml_output(of_graphml);
-                // of_graphml.close();
-                return 0;
+                }
             }
         }
+        std::cout << "Drawing "<< std::to_string(i) << " finished." << std::endl;
 NEXT_JSON:;
     }
-    std::cout << "Found no solutions with k = " << klim << " for split = " << split << std::endl;
+    std::cout << "Found no extendable solutions with k = " << klim << " for split = " << split << std::endl;
     return 0;
 }
